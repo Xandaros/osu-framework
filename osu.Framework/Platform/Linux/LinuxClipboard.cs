3c4a44aed2b4078432328b50681d0b83e13651b9
Martin Zeller <mz.bremerhaven@gmail.com>
2018-05-22 20:14:41 +0200

    Clipboard pasting for Linux

--- osu.Framework/Platform/Linux/LinuxClipboard.cs
+++ osu.Framework/Platform/Linux/LinuxClipboard.cs
@@ -2,6 +2,10 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
 // using System.Windows.Forms;
+using System;
+using System.Runtime.InteropServices;
+
+using static osu.Framework.Platform.Linux.Native.X11;
 
 namespace osu.Framework.Platform.Linux
 {
@@ -9,7 +13,46 @@ namespace osu.Framework.Platform.Linux
     {
         public override string GetText()
         {
-            return string.Empty;
+            IntPtr display = XOpenDisplay(IntPtr.Zero);
+            if (display == IntPtr.Zero)
+            {
+                return string.Empty;
+            }
+
+            int screen = XDefaultScreen(display);
+            ulong root = XRootWindow(display, screen);
+            ulong sel = XInternAtom(display, "CLIPBOARD", false);
+            ulong utf8 = XInternAtom(display, "UTF8_STRING", false);
+
+            ulong owner = XGetSelectionOwner(display, sel);
+            if (owner == 0)
+            {
+                return string.Empty;
+            }
+
+            ulong targetWindow = XCreateSimpleWindow(display, root, -10, -10, 1, 1, 0, 0, 0);
+            XSelectInput(display, targetWindow, (ulong)SelectionNotify);
+
+            ulong targetProp = XInternAtom(display, "CLIPBOARD", false);
+            XConvertSelection(display, sel, utf8, targetProp, targetWindow, CurrentTime);
+
+            XEvent xEvent = new XEvent();
+            while (true)
+            {
+                xEvent.getEvent(display);
+                if (xEvent.xevent_.type == SelectionNotify)
+                {
+                    XSelectionEvent ev = (XSelectionEvent)Marshal.PtrToStructure(xEvent.handle, typeof(XSelectionEvent));
+                    if (ev.property == 0)
+                    {
+                        return string.Empty;
+                    }
+                    string ret = GetUTF8Property(display, targetWindow, targetProp);
+                    XDestroyWindow(display, targetWindow);
+                    return ret;
+                }
+            }
+
             // return System.Windows.Forms.Clipboard.GetText(TextDataFormat.UnicodeText);
         }
 
---
+++ osu.Framework/Platform/Linux/Native/X11.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace osu.Framework.Platform.Linux.Native
+{
+    public unsafe class X11
+    {
+        public static int SelectionNotify = 31;
+        public static ulong CurrentTime = 0L;
+        public static ulong AnyPropertyType = 0L;
+
+        [DllImport("libX11.so")]
+        public extern static IntPtr XOpenDisplay(IntPtr ptr);
+        [DllImport("libX11.so")]
+        public extern static int XDefaultScreen(IntPtr display);
+        [DllImport("libX11.so")]
+        public extern static ulong XRootWindow(IntPtr display, int screen);
+        [DllImport("libX11.so")]
+        public extern static ulong XInternAtom(IntPtr display, string name, bool only_if_exists);
+        [DllImport("libX11.so")]
+        public extern static ulong XGetSelectionOwner(IntPtr display, ulong atom);
+        [DllImport("libX11.so")]
+        public extern static ulong XCreateSimpleWindow(IntPtr display, ulong root, int x, int y, uint width, uint height, uint border_width, ulong border,
+                                                       ulong background);
+        [DllImport("libX11.so")]
+        public extern static int XSelectInput(IntPtr display, ulong window, ulong event_mask);
+        [DllImport("libX11.so")]
+        public extern static int XConvertSelection(IntPtr display, ulong selection, ulong target, ulong property, ulong window, ulong time);
+        [DllImport("libX11.so")]
+        public extern static int XNextEvent(IntPtr display, IntPtr destination);
+        [DllImport("libX11.so")]
+        public extern static int XGetWindowProperty(IntPtr display, ulong window, ulong property, long offset, long length, bool delete, ulong req_type,
+                                                    UIntPtr actual_type_return, IntPtr actual_format_return, UIntPtr nitems_return,
+                                                    UIntPtr bytes_after_return, IntPtr* prop_return);
+        [DllImport("libX11.so")]
+        public extern static int XFree(IntPtr data);
+        [DllImport("libX11.so")]
+        public extern static int XDeleteProperty(IntPtr display, ulong window, ulong property);
+        [DllImport("libX11.so")]
+        public extern static int XDestroyWindow(IntPtr display, ulong window);
+
+        public static string GetUTF8Property(IntPtr display, ulong window, ulong property)
+        {
+            ulong type, dul, size, da;
+            int di;
+            IntPtr data;
+            XGetWindowProperty(display, window, property, 0, 0, false, AnyPropertyType, (UIntPtr)(&type), (IntPtr)(&di), (UIntPtr)(&dul), (UIntPtr)(&size), &data);
+            XFree(data);
+
+            ulong incr = XInternAtom(display, "INCR", false);
+            if (type == incr)
+            {
+                // Clipboard contents are huge - ignore
+                return string.Empty;
+            }
+
+            XGetWindowProperty(display, window, property, 0, (long)size, false, AnyPropertyType, (UIntPtr)(&da), (IntPtr)(&di), (UIntPtr)(&dul), (UIntPtr)(&dul), &data);
+            string ret2 = Marshal.PtrToStringAuto(data);
+            XFree(data);
+
+            return ret2;
+        }
+
+        public class XEvent
+        {
+            public IntPtr handle { get; set; }
+            public XEvent_ xevent_ { get; set; }
+
+            public XEvent()
+            {
+                handle = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(XEvent_)));
+            }
+
+            ~XEvent()
+            {
+                Marshal.FreeHGlobal(handle);
+            }
+
+            public int getEvent(IntPtr display)
+            {
+                int status = XNextEvent(display, handle);
+                xevent_ = (XEvent_)Marshal.PtrToStructure(handle, typeof(XEvent_));
+                return status;
+            }
+
+            [StructLayout(LayoutKind.Sequential, Size = 192)]
+            public struct XEvent_
+            {
+                public int type;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct XSelectionEvent
+        {
+            public int type;
+            public ulong serial;
+            public bool send_event;
+            public IntPtr display;
+            public ulong window;
+            public ulong selection;
+            public ulong target;
+            public ulong property;
+            public ulong time;
+        }
+
+    }
+}
